cfc9c329ebd6c217d1bcbeb5cd3dd743e3b73e19
Wolfgang Schmid <wolfgang.schmid@aau.at>
2016-04-04 17:01:54 +0000

    Extended Tests for Hitting Set
    git-svn-id: https://ls13-svn.cs.tu-dortmund.de/svn/exquisite/branches/aau-mod@119 5e811628-a4d4-e8de-ee5a-d3d0e172a9b0

--- diagnosis/src/test/java/org/exquisite/core/engines/query/qc/heuristic/TestHittingSet.java
+++ diagnosis/src/test/java/org/exquisite/core/engines/query/qc/heuristic/TestHittingSet.java
@@ -21,6 +21,34 @@ import static org.junit.Assert.*;
 public class TestHittingSet {
 
     @Test
+    public void testHittingSetWithZeroTimeout() {
+        Map<Integer, Float> formulaWeights = new HashMap<>();
+        formulaWeights.put(1,0.2f);
+        formulaWeights.put(2,0.3f);
+        formulaWeights.put(3,0.1f);
+        formulaWeights.put(4,0.25f);
+        formulaWeights.put(5,0.6f);
+        formulaWeights.put(7,0.7f);
+
+
+        final Set<Set<Integer>> setOfMinTraits = getSet(getSet(1,2,3), getSet(3,5), getSet(1,4), getSet(4,7));
+
+        Set<Set<Integer>> hittingSetMinCardinality = HittingSet.hittingSet(setOfMinTraits, 0, 4, 5, new MinQueryCardinality<>());
+        assertTrue(4 <= hittingSetMinCardinality.size());
+
+        hittingSetMinCardinality = HittingSet.hittingSet(setOfMinTraits, 0, 4, 4, new MinQueryCardinality<>());
+        assertTrue(4 == hittingSetMinCardinality.size());
+
+
+        Set<Set<Integer>> hittingSetMinSumFormulaWeights = HittingSet.hittingSet(setOfMinTraits, 0, 3, 5, new MinSumFormulaWeights<>(formulaWeights));
+        assertTrue(3 <= hittingSetMinSumFormulaWeights.size());
+
+        Set<Set<Integer>> hittingSetMinMaxFormulaWeights = HittingSet.hittingSet(setOfMinTraits, 0, 2, 5, new MinMaxFormulaWeights<>(formulaWeights));
+        assertTrue(2 <= hittingSetMinMaxFormulaWeights.size());
+
+    }
+
+    @Test
     public void testHittingSet() {
         Map<Integer, Float> formulaWeights = new HashMap<>();
         formulaWeights.put(1,0.2f);
@@ -69,6 +97,7 @@ public class TestHittingSet {
             hittingSetMinMaxFormulaWeights = HittingSet.hittingSet(setOfMinTraits, timeout, min, min, new MinMaxFormulaWeights<>(formulaWeights));
             assertEquals(5, hittingSetMinMaxFormulaWeights.size());
         }
+
 
     }
 
@@ -210,7 +239,15 @@ public class TestHittingSet {
         // 4
         generalTest(HittingSet.hittingSet(conflictSets, timeout, 4, 4, sortCriterion), 4, getSet(getSet("M1"), getSet("A1"), getSet("M2", "A2"), getSet("M2", "M3")));
     }
+/*
+    @Test(timeout=72000)
+    public void testExpensiveTestCase() {
+        final Set<Set<Integer>> setOfMinTraits = getSet(getSet(1,2,3,4,5), getSet(6,7,8,9,10), getSet(11,12,13,14,15), getSet(16,17,18,19,20,21,22,23), getSet(24,25,26,27,28,29,30), getSet(31,32,33,34,35,36,37,38,39,40));
 
+        Set<Set<Integer>> hittingSetMinCardinality = HittingSet.hittingSet(setOfMinTraits, 70000, 1, 500000, new MinQueryCardinality<>());
+        System.out.println(hittingSetMinCardinality.size());
+    }
+*/
     private void generalTest(Set hittingSet, int expectedSize, Set expectedResult) {
         assertNotNull(hittingSet);
         assertEquals(expectedSize, hittingSet.size());
