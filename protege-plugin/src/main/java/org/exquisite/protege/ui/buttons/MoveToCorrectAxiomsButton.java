7ab22aed8d86849e5b460760eb158f1887ea5f7c
Wolfgang Schmid <wolfgang.schmid@aau.at>
2016-08-26 18:28:57 +0200

    - Implementation of #41 - removed unused icons

--- protege-plugin/src/main/java/org/exquisite/protege/ui/buttons/MoveToCorrectAxiomsButton.java
+++ protege-plugin/src/main/java/org/exquisite/protege/ui/buttons/MoveToCorrectAxiomsButton.java
@@ -10,13 +10,35 @@ import java.util.List;
 /**
  * A button to move a selected axiom from the set of possibly faulty axioms to the set of correct axioms.
  */
-public class MoveToCorrectAxiomsButton extends AbstractImageButton {
+public class MoveToCorrectAxiomsButton extends AbstractAnswerButton {
 
     public MoveToCorrectAxiomsButton(AxiomListItem axiomListItem, InputOntologyView inputOntologyView) {
-        super("Assume as Correct", Color.GREEN.darker(),"notification_done.png", e -> {
+        super("Assume as Correct", Color.GREEN.darker(), e -> {
             List<AxiomListItem> selectedValues = new LinkedList<>();
             selectedValues.add(axiomListItem);
             inputOntologyView.getEditorKitHook().getActiveOntologyDiagnosisSearcher().moveToToCorrectAxioms(selectedValues);
         });
     }
+
+    @Override
+    public void paintButtonContent(Graphics2D g) {
+        int size = getBounds().height;
+        int thickness = (Math.round(size / 8.0f) / 2) * 2;
+
+        int x = getBounds().x;
+        int y = getBounds().y;
+
+        int insetX = size / 4;
+        int insetY = size / 4;
+        int insetHeight = size / 2;
+        int insetWidth = size / 2;
+        g.fillRect(x + size / 2  - thickness / 2, y + insetY, thickness, insetHeight);
+        g.fillRect(x + insetX, y + size / 2 - thickness / 2, insetWidth, thickness);
+    }
+
+    @Override
+    protected int getSizeMultiple() {
+        return 4;
+    }
+
 }
