6f6dddcae9cdd7b44c5e60d07f42fdda9bfed670
jaccovs <teriyakki@hotmail.com>
2018-05-18 15:02:17 +0200

    unit

--- owl-plugin/src/main/java/org/exquisite/core/diagnosis/DebuggerTest.java
+++ owl-plugin/src/main/java/org/exquisite/core/diagnosis/DebuggerTest.java
@@ -22,6 +22,8 @@ public class DebuggerTest {
         OWLObjectProperty drives = df.getOWLObjectProperty(IRI.create("http://owl.api.toyExample#Drives"));
         OWLClassExpression drivesSomeCar = df.getOWLObjectSomeValuesFrom(drives, car);
 
+        System.out.println("The number of Abox Repairs = " + "\n");
+
         OWLClass mechanic = df.getOWLClass(IRI.create("http://owl.api.toyExample#Human"));
         OWLIndividual max = df.getOWLNamedIndividual(IRI.create("http://owl.api.toyExample#Max"));
         OWLIndividual rb14 = df.getOWLNamedIndividual(IRI.create("http://owl.api.toyExample#rb14"));
