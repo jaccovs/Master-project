db4d32d498ac90cc8599810afcad4b7591243461
jaccovs <teriyakki@hotmail.com>
2018-05-18 15:28:15 +0200

    unit

--- diagnosis/src/main/java/org/exquisite/core/engines/InverseDiagnosisEngine.java
+++ diagnosis/src/main/java/org/exquisite/core/engines/InverseDiagnosisEngine.java
@@ -115,6 +115,7 @@ public class InverseDiagnosisEngine<F> extends AbstractDiagnosisEngine<F> {
 
     private Set<Diagnosis<F>> recDepthFirstSearch(InverseQuickXPlain<F> inverseQuickXPlain, Set<Diagnosis<F>> diagnoses, List<F> path) throws DiagnosisException {
         final int diagsSize = diagnoses.size();
+        System.out.println(diagsSize);
 
         if (diagsSize > sizeAlreadyFoundDiagnoses) {
             sizeAlreadyFoundDiagnoses = diagsSize;
